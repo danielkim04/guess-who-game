5025f58b98e8ce8850e24a8bd25a01e920296aac
Daniel Kim <161098788+danielkim04@users.noreply.github.com>
2024-09-12 18:08:50 +1200

    initialise crime scene fxml file and corresponding controller.

---
+++ src/main/java/nz/ac/auckland/se206/controllers/CrimeSceneController.java
@@ -0,0 +1,79 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import nz.ac.auckland.se206.GameStateContext;
+
+/**
+ * Controller class for the room view. Handles user interactions within the room where the user can
+ * chat with customers and guess their profession.
+ */
+public class CrimeSceneController {
+
+  @FXML private Rectangle rectClueBag;
+  @FXML private Rectangle rectClueBook;
+  @FXML private Rectangle rectClueNote;
+  @FXML private Label labelTimer;
+  @FXML private Button btnGuess;
+
+  private static boolean isFirstTimeInit = true;
+  private static GameStateContext context = new GameStateContext();
+
+  /**
+   * Initializes the room view. If it's the first time initialization, it will provide instructions
+   * via text-to-speech.
+   */
+  @FXML
+  public void initialize() {
+    // to be implemented
+  }
+
+  /**
+   * Handles the key pressed event.
+   *
+   * @param event the key event
+   */
+  @FXML
+  public void onKeyPressed(KeyEvent event) {
+    System.out.println("Key " + event.getCode() + " pressed");
+  }
+
+  /**
+   * Handles the key released event.
+   *
+   * @param event the key event
+   */
+  @FXML
+  public void onKeyReleased(KeyEvent event) {
+    System.out.println("Key " + event.getCode() + " released");
+  }
+
+  /**
+   * Handles mouse clicks on rectangles representing people in the room.
+   *
+   * @param event the mouse event triggered by clicking a rectangle
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleRectangleClick(MouseEvent event) throws IOException {
+    Rectangle clickedRectangle = (Rectangle) event.getSource();
+    // to be implemented
+  }
+
+  /**
+   * Handles the guess button click event.
+   *
+   * @param event the action event triggered by clicking the guess button
+   * @throws IOException if there is an I/O error
+   */
+  @FXML
+  private void handleGuessClick(ActionEvent event) throws IOException {
+    context.handleGuessClick();
+  }
+}
